d2e09e9c41a553b6216eabe6bfac01e5b939b85b
Evan Hahn <me@evanhahn.com>
2013-05-11 11:38:12 -0700

    Initial commit

---
+++ src/foo.ts
@@ -0,0 +1,15 @@
+// Put your TypeScript here.
+
+class Foo {
+
+	private bar: string;
+
+	getBar(): string {
+		return this.bar;
+	}
+
+	setBar(value: string): void {
+		this.bar = value;
+	}
+
+}
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+// Put your TypeScript here.
+
+/// <reference path="foo.ts" />
+
+var foo = new Foo;
+foo.setBar("Hello world!");
+console.log(foo.getBar());
